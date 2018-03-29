7dae5b41c13293275a5959cfe063478273c1657e
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-03-29 14:06:16 -0600

    Fix blog build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #nightly build script
 echo ".....Building blog site....."
-mkdocs build; rsync -ir site/ $1/;
+mkdocs build; mkdir -p $1; rsync -ir ./site/ $1/;
 echo "....Completed building blog site...."
